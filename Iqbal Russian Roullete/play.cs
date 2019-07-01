0eeb3823e091ca54e2ce55d09005a8e14b6421b3
07ekb <07ekb@LAPTOP-HMFBP9KQ>
2019-07-02 10:11:28 +1200

    Add project files.

---
+++ Iqbal Russian Roullete/play.cs
@@ -0,0 +1,27 @@
+﻿using iqbal_russian;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Iqbal_Russian_Roullete
+{
+    public partial class play : Form
+    {
+        public play()
+        {
+            InitializeComponent();
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            (new Russian()).Show();
+            this.Hide();
+        }
+    }
+}
